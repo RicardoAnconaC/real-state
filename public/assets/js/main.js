05ce9a88cbea1b1253f0a39274b5bcad065dc453
Ricardo Ancona <ricardo.ancona22@gmail.com>
2021-01-02 10:19:34 -0600

    Add search forms

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -11,3 +11,21 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+function openContainer(evt, condition) {
+
+  var i, x, tablinks;
+
+  x = document.getElementsByClassName("search-form");
+  for (i = 0; i < x.length; i++) {
+    x[i].classList.add("hidden");
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" bg-blue-400", "");
+  }
+
+  document.getElementById(condition).classList.replace("hidden", "block");
+  evt.currentTarget.className += " bg-blue-400";
+}
