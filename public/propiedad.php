f99f31507093ac0de727579bebe070cd0b6036fb
Ricardo Ancona <ricardo.ancona22@gmail.com>
2021-01-03 15:50:24 -0600

    Add pripiedad_swiper.php

--- public/propiedad.php
+++ public/propiedad.php
@@ -6,37 +6,14 @@
   <title>INmobi | Casa en Villas Altas</title>
   <link rel="stylesheet" href="./assets/css/swiperjs.css">
   <style>
-    .swiper-container {
-      width: 100%;
-      height: 300px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-
-    .swiper-slide {
-      background-size: cover;
-      background-position: center;
-    }
-
-    .gallery-top {
-      height: 80%;
-      width: 100%;
-    }
-
-    .gallery-thumbs {
-      height: 20%;
-      box-sizing: border-box;
-      padding: 10px 0;
-    }
-
-    .gallery-thumbs .swiper-slide {
-      width: 25%;
-      height: 100%;
-      opacity: 0.4;
-    }
-
     .gallery-thumbs .swiper-slide-thumb-active {
       opacity: 1;
+    }
+    .swiper-button-prev:after,
+    .swiper-button-next:after {
+      font-size: 2rem;
+      font-weight: 900;
+      color: #3B82F6;
     }
   </style>
   <link rel="stylesheet" href="./assets/css/style.css">
@@ -91,33 +68,7 @@
 
   <main class="bg-gray-100 h-screen mb-24">
     <!-- Swiper -->
-    <div class="swiper-container gallery-top">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/001.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/002.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/003.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/004.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/005.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/001.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/002.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/003.jpg)"></div>
-      </div>
-      <!-- Add Arrows -->
-      <div class="swiper-button-next swiper-button-white"></div>
-      <div class="swiper-button-prev swiper-button-white"></div>
-    </div>
-    <div class="swiper-container gallery-thumbs">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/001.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/002.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/003.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/004.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/005.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/001.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/002.jpg)"></div>
-        <div class="swiper-slide" style="background-image:url(./assets/images/propiedad/003.jpg)"></div>
-      </div>
-    </div>
+    <?php include 'propiedad_swiper.php'; ?>
   </main>
 
   <!-- Footer -->
